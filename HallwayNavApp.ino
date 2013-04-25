154695391b684f64b5cc4e92cb1f958c37e9ce47
Sean Cardello <seanmc602@gmail.com>
2013-04-25 03:06:51 -0400

    - Matt's App
    Signed-off-by: Sean Cardello <seanmc602@gmail.com>

--- HallwayNavApp.ino
+++ HallwayNavApp.ino
@@ -5,22 +5,34 @@ float forward;//, previous_forward;
 float right;//, previous_right;
 float backward;//, previous_backward;
 float left;//, previous_left;
+float ir_dif_error;
 int sonar;//, previous_sonar;
-float heading;//, previous_heading;
+int heading;//, previous_heading;
 float height;//height of user
 boolean obstacle_avoidance_left = false;//try and go left around object
 boolean obstacle_avoidance_right = false;//try and go right around object
+boolean roam;
 const float error = .05;//5% error checking 
 const int ir_max = 60;//max ir value filtering?
 const int average = 5;//number of values to average
 byte previous_intersection[2];//[0] most recent, [1] before that
-
+float two_feet = 24;
+byte intersection_compare;
 boolean repeat;
 
-static int steps = 0;
+enum state{
+  ROAM,
+  HALLWAY,
+  INTERSECTION,
+  GOAL,
+} mode;
 
 void setup()
 {
+  float temp1;
+  float temp2;
+  float temp3;
+  
   Serial.begin(9600);
   compass_init();
   sonar_init();
@@ -29,84 +41,187 @@ void setup()
   ir_init();
   
   // get_sensor_data(); //for initial localize
-  //call take_a_step(direction) to line up with major direction;
   //localize();
   
   previous_intersection[0] = B1111;
   previous_intersection[1] = B1111;
   repeat = true;
+  mode = ROAM;
+  ir_dif_error = error * ir_max;
+
+  get_ir_average();
+  temp1 =  max(right, left);
+  temp2 = max(forward, backward);
+//  temp3 = max(temp1, temp2);
+  if(forward == max(temp1, temp2))
+  {
+    direct(0);
+  }
+  else if(left == max(temp1, temp2))
+  {
+    direct(2700);
+  }
+  else if(right == max(temp1, temp2))
+  {
+    direct(900);
+  }
+  else //if(backward == max(temp1, temp2))
+  {
+    direct(1800);
+  }
+  delay(500);
+  
+  direct(-1); //turn the headband off
+  delay(2000);
 }
 
 void loop()
 {
-  //get_sensor_data();//do this indiviually?
+  int new_heading;
+  float difference;
+  int degree;
   
-  Serial.println(sonar_object());
-  
-  /*
-  heading = 0;
-  if(!at_goal())
+  if(mode == ROAM)
   {
-    //main loop!
-    if(repeat)
-    {
-      //for(int i = 0; i < 100; i++)
-      { 
-        heading = compass_read();
-        delay(50);
-      }
-      
-      Serial.println(heading);
-    }
+    Serial.println("ROAM");
     
-    /*
-    //determine heading
-    //make left = right ie. right/left should be 1
-    for(int i = 0; i < 100; i++)
-    {  
-      left += ir_read('L');
-      right += ir_read('R');
-    }
-    left = left/100;
-    right = right/100;
-    
-    Serial.print("left distance ");
-    Serial.println(left);
-    Serial.print("right distance ");
-    Serial.println(right);
-    //take_a_step();
-    
+    get_ir_average();
     difference = right - left;
-    int degree = map(abs(difference), 0, 60, 0, 900);
-    if(difference > 0) //closer to left
+    
+    //both hallways present, try and center
+    degree = map(abs(difference), 0, 60, 0, 450);
+    if(forward < 24)  //if true -> wall in front
+    {
+      direct(-1);  //stop
+      if(abs(difference) < ir_dif_error)  //dead end, thus intersection
+      {
+        direct(1800);        
+        delay(500);
+        direct(-1);
+        mode = INTERSECTION;
+      }
+      else if(difference > 0)  //difference > ir_dif_error)  //right>left, go right
+      {
+           direct(450);
+           delay(500);
+           direct(-1);
+      }
+      else if(difference > 0)  //difference > ir_dif_error)  //right>left, go right
+      {
+           direct(3150);
+           delay(200);
+           direct(-1);
+      }
+      else  //default turn around....
+      {
+        direct(1800);
+        delay(200);
+        direct(-1);
+      }
+    }
+    else  //no wall in front
+    {
+      if(abs(difference) < ir_dif_error  && (right < ir_max || left < ir_max))
+      {
+        direct(-1);
+        mode = HALLWAY;
+       
+      }
+      else if(difference > 0) //right>left, closer to left
+      {
+        direct(degree);
+        Serial.print("difference>0 : ");
+        Serial.println(degree);
+      }
+      else if(difference < 0) //closer to right
+      {
+        direct(3600 - degree);
+        Serial.print("difference<0 : ");
+        Serial.println(3600 - degree);
+      }
+      else //middle
+      {
+        Serial.println("middle");
+        direct(0);
+      }
+    }
+  }
+  else if(mode == HALLWAY)
+  {
+    Serial.println("HALLWAY");
+    //direct(-1);
+    
+    get_ir_average();
+    difference = right - left;
+    
+    //both hallways present, try and center
+    degree = map(abs(difference), 0, 60, 0, 300);
+    
+    if(right == ir_max && left > two_feet)// no wall on right
+    {
+      direct(-1);
+      mode = INTERSECTION;
+    }
+    else if(left == ir_max && right > two_feet) //no wall on left
+    {
+      direct(-1);
+      mode = INTERSECTION; 
+    }
+    else // not at intersection
+    {
+      if(difference > 0) //right>left, closer to left
+      {
+        direct(degree);
+        Serial.print("difference>0 : ");
+        Serial.println(degree);
+      }
+      else if(difference < 0) //closer to right
+      {
+        direct(3600 - degree);
+        Serial.print("difference<0 : ");
+        Serial.println(3600 - degree);
+      }
+      else //middle
+      {
+        Serial.println("middle");
+        direct(0);
+      }
+    }  
+  }  
+  else if(mode == INTERSECTION)
+  {
+    Serial.println("INTERSECTION");
+    direct(-1);
+    delay(150);
+    direct(900);
+    delay(150);
+    direct(2700);
+    delay(150);
+    
+    intersection_compare = B0000;
+    heading = compass_read();
+    //iterate through the map orientations
+    for(int i = 0; i < 19; i++)
+    {
+       
+    }
+    /* STOPPED HERE!
+    get_ir_average();
+    if(front == ir_max)
     {
       
-      direct(degree);
-      Serial.print("difference>0 : ");
-      Serial.println(degree);
     }
-    else if(difference < 0) //closer to right
-    {
-      direct(3600 - degree);
-      Serial.print("difference<0 : ");
-      Serial.println(3600 - degree);
-    }
-    else //middle
-    {
-      Serial.println("middle");
-      direct(0);
-    }
+    */
+    
+    
+    
   }
-  else
+  else if(mode == GOAL) //GOAL!!!!!
   {
-    //at goal, signal done!
-    //vibrate motors in a circle to signal done
+      Serial.println("GOAL!");
   }
-  //add a delay
-  delay(1000);
-  */
 }
-
+    
 void get_sensor_data()
 {
   //read into a struct?
@@ -120,63 +235,21 @@ void get_sensor_data()
   //accel?  //part of take a step function instead?
 }
 
-void localize()
-{
-  //use sensor data and previous intersections to guess location
-  //scan through list
-   
-  
-}
-
-boolean take_a_step(int angle)
-{
-  //return true if step taken
-  //return false if not
-  //get accel data
-  //reisue command for some number of times to get user to progress
-  //delap between each loop
-  return true;
-
-}
-boolean following_path()
-{
-  //if not following path
-  //put in wander mode
-  return false; 
-}
-
-boolean obstacle()
-{
-  //check to see if there is an obstacle ahead
-  if(sonar < height*(1-error))  //error needs to be smaller to detect small obstacles?
-  {
-    //obstacle!
-    //set obstacle avoidance flag left 
-    //or obstacle avoidance flag right 
-    return true;
+void get_ir_average()  //average 100 ir readings
+{   
+  left = 0;
+  right = 0;
+  forward = 0;
+  backward = 0;
+  for(int i = 0; i < 100; i++)
+  {  
+    left += ir_read('L');
+    right += ir_read('R');
+    forward += ir_read('F');
+    backward += ir_read('B');
   }
-  return false;
-}
-
-boolean at_intersection()  //note: deadend also considered intersection
-{
-  //compare byte value of intersection to the map
-  //NESW  relative to the map
-  //N = north, E = east, etc.
-  //set previous intersections
- // previous_intersection[1] = previous_intersection[0];
-  //previous_intersection[0] = this intersection
-  return false;
-}
-
-boolean at_goal()
-{
-  //requirements to be at goal:
-  //at a hallway B1010 for open door
-  //at a intersection B1110 for closed door
-  //previous intersection was B0001
-  //intersection before that was either B0000 or B0100
-  //compass facing east(relative to map)
-  //distance traveled from last intersection is about 288
-  return false; 
+  left = left/100;
+  right = right/100;
+  forward = forward/100;
+  backward = backward/100;
 }
